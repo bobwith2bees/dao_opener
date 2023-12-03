3b19887f15337842d24cb1d6ec8d9e3f8bdb145b
Bob Black <bob@fifthpint.com>
2023-12-02 20:02:54 -0600

    Check back in cloud files, now with iniital calls to Polygon validation. Test push to main and build.

--- lib/custom_code/actions/add_to_wallet.dart
+++ lib/custom_code/actions/add_to_wallet.dart
@@ -10,14 +10,37 @@ import 'package:flutter/material.dart';
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
 import 'package:flutter_wallet_card/flutter_wallet_card.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 Future addToWallet(FFUploadedFile? passFile) async {
   /// It returns [PasskitFile] object
-  final passkitFile = await FlutterWalletCard.generateFromFile(
-    id: 'example-pass',
-    //file: File('path-to-file.pkpass'),
-    file: passFile.file,
-  );
 
+  if (Platform.isAndroid) {
+    print('package does not support Google Passes yet');
+    return;
+  }
+
+  if (passFile == null) {
+    print('addToWallet - No file to add.');
+    return;
+  } else {
+    print('addToWallet - name: ${passFile.name}, bytes: ${passFile.bytes?.length ?? 0}');
+  }
+
+  // Drop a copy in the appliation's document folder for save keeping, and the library flutter_wallet_card wants a file.
+  final documentsDir = await getApplicationDocumentsDirectory();
+  String documentsPath = documentsDir.path;
+
+  File passFileOnDisk = File("$documentsPath/walletPass.pkpass");
+  await passFileOnDisk.create(recursive: true);
+
+  print('addToWallet - saving to local file ${passFileOnDisk.path}');
+  passFileOnDisk.writeAsBytesSync(passFile.bytes ?? []);
+
+  print('addToWallet - extracting pass contents}');
+  final passkitFile = await FlutterWalletCard.generateFromFile(id: 'dao-opener', file: passFileOnDisk);
+
+  print('addToWallet - user clicked addPasskit to wallet}');
   final completed = await FlutterWalletCard.addPasskit(passkitFile);
 }
